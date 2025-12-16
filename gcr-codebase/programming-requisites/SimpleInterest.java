268fc223e5bdf37889adb4fbd91ba9b507f49212
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-16 22:26:19 +0530

    [Divyansh Pandey] : added a program to calculate simple interest

---
+++ gcr-codebase/programming-requisites/SimpleInterest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SimpleInterest
+{
+ public static void main(String []args)
+ {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter the principal amount");
+   double principal = sc.nextDouble();
+   System.out.println("Enter the rate of interest");
+   double rate = sc.nextDouble();
+   System.out.println("Enter the time");
+   double time = sc.nextDouble();
+
+   double simpleInterest = (principal * rate * time) / 100;
+   System.out.println("The simple interest is: " + simpleInterest);
+ }
+}
