d2248028393b53651a480cb421a6e1aa1deb839a
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-16 22:17:56 +0530

    [Divyansh Pandey] : added a program to convert the temperature from celsius to fahrenheit

---
+++ gcr-codebase/programming-requisites/CelsiusToFahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class CelsiusToFahrenheit
+{
+  public static void main(String []args)
+{
+    Scanner sc=new Scanner(System.in);
+     System.out.println("Enter Temperature in Celsius");
+     double tempInCelsius=sc.nextDouble();
+     double tempInFahrenheit= ((tempInCelsius) * 9/5)+32;
+     System.out.println("Temperature in Fahrenheit is:"+ tempInFahrenheit);
+    
+}
+}
