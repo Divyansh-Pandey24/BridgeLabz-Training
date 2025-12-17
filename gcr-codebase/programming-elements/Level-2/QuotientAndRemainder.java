d673df8be67c55252a07d619a8d5ec22039c943d
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-18 00:13:11 +0530

    [Divyansh Pandey]: Added code for Quotient and remainder

---
+++ gcr-codebase/programming-elements/Level-2/QuotientAndRemainder.java
@@ -0,0 +1,30 @@
+// Writing a code to find quotient and remainder of two numbers
+// Create QuotientAndRemainder class
+
+import java.util.Scanner;
+
+public class QuotientAndRemainder {
+ public static void main(String[] args) {
+
+  // Take input from user
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("Enter first number : ");
+  int number1 = input.nextInt();
+
+  System.out.print("Enter second number : ");
+  int number2 = input.nextInt();
+
+  // Calculate quotient and remainder
+  int quotient = number1 / number2;
+  int remainder = number1 % number2;
+
+  // Display the result
+  System.out.println("The Quotient is " + quotient +
+                     " and Remainder is " + remainder +
+                     " of two numbers " + number1 +
+                     " and " + number2);
+
+  input.close();
+ }
+}
