34035ed572b9ff2dac6ba0948f9f5b6868333a76
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-17 23:12:12 +0530

    [Divyansh Pandey]: Added a program for distance conversion from kilometers to miles

---
+++ gcr-codebase/programming-elements/Level-1/KilometersToMiles.java
@@ -0,0 +1,18 @@
+// Writing a program to convert the distance from kilometers to miles
+
+// Create KilometersToMiles class to convert the distance from kilometers to miles
+public class KilometersToMiles
+{
+  public static void main(String []args)
+  {
+    // Create a variable to indicate the distance in kilometer
+     double disInKilo=10.8;
+      
+    // Create a variable to convert the distance in Miles
+    double disInMiles= disInKilo*(1.6);
+    
+    // Print the distance in Miles
+     System.out.println("The distance 10.8km in miles is :"+ disInMiles);
+  }
+
+}
