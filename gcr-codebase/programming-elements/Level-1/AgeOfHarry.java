9aaa1459ce03fbee763597d1b63baaad350ff1eb
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-17 17:24:31 +0530

    [Divyansh Pandey] : Added a program for age of harry

---
+++ gcr-codebase/programming-elements/Level-1/AgeOfHarry.java
@@ -0,0 +1,21 @@
+// writing a Java Code to find out the age of Harry.
+
+// Create AgeOfHarry Class to find out tht age of Harry.
+public class AgeOfHarry
+{
+ public static void main(String []args)
+ {
+    // create a variable to indicate Harry's birth year.
+    int birthYear= 2000;
+    
+   // create a variable to indicate current year.
+    int currentYear= 2024;
+  
+   // create a variable to indicate Harry's age.
+    int age= currentYear-birthYear;
+   
+    // Print the age of Harry.
+    System.out.println("Harry's age in 2024 is :" + age);
+  }
+
+}
