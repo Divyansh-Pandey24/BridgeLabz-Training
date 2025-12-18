e3540a648b6da590585f4a209a8139c5fd4a01ce
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-18 19:54:10 +0530

    [Divyansh Pandey]: Added a program to find out the sum of numbers

---
+++ gcr-codebase/control-flow/Level-1/SumOfNumbers.java
@@ -0,0 +1,33 @@
+// Writing a code to find the sum of numbers until the user enters 0
+// Create SumOfNumbers class
+
+import java.util.Scanner;
+
+public class SumOfNumbers { 
+
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        // Create a variable to store the total
+        double total=0.0;
+
+        // Using while loop to calculate the sum util user enters 0
+        while(true) {
+           
+        System.out.println("Enter the number: ");
+        double number=input.nextDouble();
+        
+        // Using if block to check the user's input
+        if(number==0) {
+         break;
+        }
+        
+        // Adding the number in the total
+        total=total+number;
+        }
+  
+        // Displaying the result
+        System.out.println(total);
+   
+    }
+}
