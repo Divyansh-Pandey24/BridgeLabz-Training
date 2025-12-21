34ca1f73690d0af2d3e41c406cdc2b76ca4d0b94
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-21 21:44:48 +0530

    [Divyansh Pandey]: Added program to check voting eligibility of students based on age

---
+++ gcr-codebase/methods/Level-2/StudentVoteChecker.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class StudentVoteChecker {
+
+    // Method to check whether a student can vote
+    public boolean canStudentVote(int age) {
+
+        if (age < 0) {
+            return false;
+        }
+
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Take input from users
+        Scanner sc = new Scanner(System.in);
+        StudentVoteChecker checker = new StudentVoteChecker();
+
+        int[] ages = new int[10];
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = sc.nextInt();
+
+            boolean canVote = checker.canStudentVote(ages[i]);
+
+            if (canVote) {
+                System.out.println("Student " + (i + 1) + " can vote");
+            } else {
+                System.out.println("Student " + (i + 1) + " cannot vote");
+            }
+        }
+    }
+}
