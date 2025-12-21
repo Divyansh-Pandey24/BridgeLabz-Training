1091541388040772c2def350555cc362a4ddc7a0
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-21 21:23:29 +0530

    [Divyansh Pandey]: Added program to calculate wind chill temperature

---
+++ gcr-codebase/methods/Level-1/WindChillTemperature.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WindChillTemperature {
+
+    // Method to calculate the temperature
+    public static double calculateWindChill(double temp, double windSpeed) {
+        return 35.74 + 0.6215 *temp + (0.4275*temp - 35.75) * Math.pow(windSpeed,0.16);
+    }
+
+    public static void main(String[] args) {
+
+        // Take input from the user
+        Scanner sc=new Scanner(System.in);
+        double temp= sc.nextDouble();
+        double windSpeed= sc.nextDouble();
+
+
+        // Calling the method
+        double windChillTemperature= calculateWindChill(temp, windSpeed);
+        
+        // Displaying the results
+        System.out.println("The wind chill temperature: "+windChillTemperature);
+        
+    }
+}
