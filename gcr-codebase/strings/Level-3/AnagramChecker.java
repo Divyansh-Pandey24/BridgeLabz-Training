f1f2911d02e23d01f03a5b5f41f008f3fb41df23
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-23 17:06:15 +0530

    [Divyansh Pandey]: Added a program to check anagram

---
+++ gcr-codebase/strings/Level-3/AnagramChecker.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class AnagramChecker {
+
+    // Method to check if two texts are anagrams
+    public static boolean isAnagram(String text1, String text2) {
+
+        if (text1.length() != text2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[256];
+        int[] freq2 = new int[256];
+
+        for (int i = 0; i < text1.length(); i++) {
+            freq1[text1.charAt(i)]++;
+        }
+
+        for (int i = 0; i < text2.length(); i++) {
+            freq2[text2.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        // Take input from the users
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first text: ");
+        String text1 = sc.nextLine();
+        System.out.print("Enter second text: ");
+        String text2 = sc.nextLine();
+
+        boolean result = isAnagram(text1, text2);
+
+        if (result) {
+            System.out.println("The given texts are anagrams");
+        } else {
+            System.out.println("The given texts are NOT anagrams");
+        }
+
+        sc.close();
+    }
+}
