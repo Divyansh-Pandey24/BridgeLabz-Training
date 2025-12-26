2dcd95d7d4ee5a06f6dafda799afdb5df6e0fe16
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-26 14:25:56 +0530

    [Divyansh Pandey]: Added a program to track body mass index

---
+++ scenario-based-codebase/Day-1/BodyMassIndexTracker.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class BodyMassIndexTracker {
+
+    public static void main(String[] args) {
+
+        // Scanner object to take input from the user
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking height input (in meters)
+        System.out.print("Enter height in meters: ");
+        double heightInMeters = scanner.nextDouble();
+
+        // Taking weight input (in kilograms)
+        System.out.print("Enter weight in kilograms: ");
+        double weightInKg = scanner.nextDouble();
+
+        // Calculating BMI using the formula
+        double bmi = weightInKg / (heightInMeters * heightInMeters);
+
+        // Displaying BMI value
+        System.out.println("Your BMI is: " + bmi);
+
+        // Determining BMI category using if-else
+        if (bmi < 18.5) {
+            System.out.println("BMI Category: Underweight");
+        } 
+        else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("BMI Category: Normal");
+        } 
+        else {
+            System.out.println("BMI Category: Overweight");
+        }
+
+        // Closing the scanner
+        scanner.close();
+    }
+}
