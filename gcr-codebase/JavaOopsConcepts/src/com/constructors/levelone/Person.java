02a1a3a9532e3116f366b6d0114de6340afbd7d3
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-27 22:56:57 +0530

    [Divyansh Pandey]: Added Person.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Person.java
@@ -0,0 +1,31 @@
+package com.constructors.levelone;
+
+public class Person {
+    String name;
+    int age;
+
+    // Parameterized constructor
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    void display() {
+        System.out.println(name + " | Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Scout", 25);
+        Person p2 = new Person(p1);
+
+        p1.display();
+        p2.display();
+    }
+}
+
