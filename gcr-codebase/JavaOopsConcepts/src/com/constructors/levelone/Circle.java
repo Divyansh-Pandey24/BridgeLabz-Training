727f1cedc440efd3529b97214f0e22acf6ed2fe2
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2025-12-27 22:53:30 +0530

    [Divyansh Pandey]: Added Circle.java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Circle.java
@@ -0,0 +1,28 @@
+package com.constructors.levelone;
+
+public class Circle {
+    double radius;
+
+    // Default constructor
+    Circle() {
+        this(1.0);   // constructor chaining
+    }
+
+    // Parameterized constructor
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(5);
+
+        System.out.println("Default radius area: " + c1.area());
+        System.out.println("Custom radius area: " + c2.area());
+    }
+}
+
