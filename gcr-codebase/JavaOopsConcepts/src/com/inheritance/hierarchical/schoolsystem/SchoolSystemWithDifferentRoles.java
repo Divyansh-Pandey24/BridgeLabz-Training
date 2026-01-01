0b7a8d05265c19958a9540bbe9624dc9c70711ea
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2026-01-01 14:39:13 +0530

    [Divyansh Pandey]: Added programs for school system with different roles

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchical/schoolsystem/SchoolSystemWithDifferentRoles.java
@@ -0,0 +1,25 @@
+package com.inheritance.hierarchical.schoolsystem;
+
+public class SchoolSystemWithDifferentRoles {
+
+    public static void main(String[] args) {
+
+        // Hierarchical inheritance demonstration
+        Person p1 = new Teacher("Mr. Mishre", 55, "Social Science");
+        Person p2 = new Student("Puri", 15, "12th Grade");
+        Person p3 = new Staff("Harsh", 45, "Administration");
+
+        p1.displayDetails();
+        p1.displayRole();
+        
+        System.out.println("---------------------");
+
+        p2.displayDetails();
+        p2.displayRole();
+        
+        System.out.println("---------------------");
+
+        p3.displayDetails();
+        p3.displayRole();
+    }
+}
