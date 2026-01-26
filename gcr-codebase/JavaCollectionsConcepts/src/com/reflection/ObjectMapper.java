586acc48747c0103155c9ca930bf2b600ed519f6
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2026-01-27 02:21:11 +0530

    [Divyansh Pandey]: Updated program for object mapper

--- gcr-codebase/JavaCollectionsConcepts/src/com/reflection/ObjectMapper.java
+++ gcr-codebase/JavaCollectionsConcepts/src/com/reflection/ObjectMapper.java
@@ -1,58 +1,18 @@
 package com.reflection;
-import java.lang.reflect.*;
+
+import java.lang.reflect.Field;
 import java.util.Map;
 
-public class CustomObjectMapper {
+public class ObjectMapper {
 
-    public static <T> T toObject(Class<T> clazz, Map<String, Object> properties) {
-        try {
-            T instance = clazz.getDeclaredConstructor().newInstance();
+    public static <T> T toObject(Class<T> clazz, Map<String, Object> map) throws Exception {
+        T obj = clazz.getDeclaredConstructor().newInstance();
 
-            for (Map.Entry<String, Object> entry : properties.entrySet()) {
-                String fieldName = entry.getKey();
-                Object value = entry.getValue();
-
-                try {
-                    Field field = clazz.getDeclaredField(fieldName);
-                    field.setAccessible(true);
-
-                    field.set(instance, value);
-
-                } catch (NoSuchFieldException e) {
-                    System.out.println("Field not found: " + fieldName);
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Type mismatch for field: " + fieldName);
-                }
-            }
-
-            return instance;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            Field field = clazz.getDeclaredField(entry.getKey());
+            field.setAccessible(true);
+            field.set(obj, entry.getValue());
         }
-    }
-
-    public static void main(String[] args) {
-        Map<String, Object> data = Map.of(
-            "name", "Divyansh",
-            "age", 20,
-            "email", "divyansh@example.com"
-        );
-
-        Person person = toObject(Person.class, data);
-
-        System.out.println(person);
-    }
-}
-
-class Person {
-    private String name;
-    private int age;
-    private String email;
-
-    @Override
-    public String toString() {
-        return "Person{name='" + name + "', age=" + age + ", email='" + email + "'}";
+        return obj;
     }
 }
