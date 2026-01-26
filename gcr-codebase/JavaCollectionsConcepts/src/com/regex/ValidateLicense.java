ec559f63257e154a5155ec61ac20bcc83207fea9
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2026-01-27 00:19:37 +0530

    [Divyansh Pandey]: Added a program license validator

---
+++ gcr-codebase/JavaCollectionsConcepts/src/com/regex/ValidateLicense.java
@@ -0,0 +1,31 @@
+package com.regex;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateLicense {
+	public static void main(String[] args) {
+		String regex = "^[A-Z]{2}[0-9]{4}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        do {
+            System.out.println("Enter License Plate number: ");
+            String input = sc.next();
+
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.matches())
+                System.out.println("Valid License Plate number");
+            else
+                System.out.println("Invalid License Plate number");
+
+            System.out.println("Do you want to continue? (y/n)");
+            choice = sc.next();
+
+        } while (choice.equalsIgnoreCase("y"));
+
+        sc.close();
+	}
+}
